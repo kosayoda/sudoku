ba5bf33a8626fedc13bf804d53a58b1362546899
kosayoda <git@siek.dev>
2022-06-29 16:27:50 -0400

    Use integer instead of bitvec for speed.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, BufWriter, Write};
 
 use color_eyre::Result;
 use owo_colors::OwoColorize;
@@ -10,45 +10,22 @@ use ukodus::solver::{search, Solver};
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let file = File::open("hard_sudokus.txt")?;
-    let reader = BufReader::new(file);
-    let lines: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
-    for line in lines {
-        // let (_board, _solution) = line.split_once(',').unwrap();
-        // println!("Board: {_board}");
-        // println!("Solution: {_solution}");
+    let reader = BufReader::new(File::open("all_17_clue_sudokus.txt")?);
 
+    let mut total_time = std::time::Duration::new(0, 0);
+    for line in reader.lines().flatten() {
         let board: Board = line.as_str().try_into()?;
+        // println!("Board: {line}");
+        // println!("{}\n{}\n", "--- Board ---".bold().green(), board);
         let solver: Solver = (&board).into();
-        let solved = search(solver).unwrap();
-        // let solution: Board = _solution.try_into()?;
-        // println!("{}\n{}\n", "--- Solved ---".bold().green(), solved);
+
+        let before = std::time::Instant::now();
+        let solution = search(solver).unwrap();
         // println!("{}\n{}\n", "-- Solution --".bold().red(), solution);
+
+        total_time += before.elapsed();
     }
+    println!("Elapsed time: {:2?}", total_time.green());
 
     Ok(())
 }
-
-//
-// fn main() -> Result<()> {
-//     color_eyre::install()?;
-//
-//     let board: Board =
-//         "000075400000000008080190000300001060000000034000068170204000603900000020530200000"
-//             .try_into()?;
-//
-//     let solution: Board =
-//         "693875412145632798782194356357421869816957234429368175274519683968743521531286947"
-//             .try_into()?;
-//
-//     let solver: Solver = (&board).into();
-//     println!("\n{}\n{}\n", "--- Board ---".bold().yellow(), board);
-//     println!("{}\n{}\n", "--- Solver ---".bold().yellow(), solver);
-//
-//     if let Some(solved) = search(solver) {
-//         println!("{}\n{}\n", "--- Solved ---".bold().green(), solved);
-//     }
-//
-//     println!("{}\n{}\n", "-- Solution --".bold().red(), solution);
-//     Ok(())
-// }
--- src/solver.rs
+++ src/solver.rs
@@ -1,58 +1,73 @@
-use std::fmt::Display;
+use std::fmt::{Debug, Display};
 
-use bitvec::prelude::*;
-use color_eyre::{eyre::eyre, Report, Result};
-use once_cell::sync::OnceCell;
+use color_eyre::{eyre::eyre, Result};
 use owo_colors::OwoColorize;
 
 use crate::board::Board;
 use crate::UnicodeBox;
 
-static ONES: OnceCell<BitArr!(for 9)> = OnceCell::new();
+static BIT_MASK: [u16; 16] = [
+    1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
+];
+
+macro_rules! iter_ones {
+    (for $bit_index:ident in $bitset:ident => $body:block ) => {
+        for $bit_index in 0..16 {
+            if $bitset & BIT_MASK[$bit_index] != 0 {
+                $body
+            }
+        }
+    };
+}
+
+type DigitMask = u16;
 
 pub fn search(board: Solver) -> Option<Solver> {
     if let Some((x, y)) = board.get_next_square_to_assign() {
         let digits = board.board[x][y];
-        for bit_index in digits[..9].iter_ones() {
+        iter_ones!(for bit_index in digits => {
             let mut board_copy = board;
             if board_copy.set_digit(x, y, bit_index).is_ok() {
                 if let Some(solution) = search(board_copy) {
                     return Some(solution);
                 }
             }
-        }
+        });
     } else {
         return Some(board);
     }
     None
 }
 
-type Digits = BitArr!(for 9);
-
 // Source: https://www.sebastiansylvan.com/post/sudoku/
 #[derive(Clone, Copy)]
 pub struct Solver {
-    board: [[Digits; 9]; 9],
-    possible_blocks: [[[Digits; 9]; 3]; 3],
-    possible_rows: [[Digits; 9]; 9],
-    possible_cols: [[Digits; 9]; 9],
+    board: [[DigitMask; 9]; 9],
+    possible_blocks: [[[DigitMask; 9]; 3]; 3],
+    possible_rows: [[DigitMask; 9]; 9],
+    possible_cols: [[DigitMask; 9]; 9],
     next_square_candidate: Option<(usize, usize)>,
 }
 
 impl Solver {
-    pub fn eliminate(&mut self, x: usize, y: usize, mut digits_to_eliminate: Digits) -> Result<()> {
+    pub fn eliminate(
+        &mut self,
+        x: usize,
+        y: usize,
+        mut digits_to_eliminate: DigitMask,
+    ) -> Result<()> {
         // Only eliminate digits that are possible in the square
         digits_to_eliminate &= self.board[x][y];
 
         // All digits to eliminate are eliminated
-        if digits_to_eliminate.not_any() {
+        if digits_to_eliminate == 0 {
             return Ok(());
         }
 
         // Eliminate the digits
-        self.board[x][y] &= !digits_to_eliminate & ONES.get().unwrap();
+        self.board[x][y] &= !digits_to_eliminate;
         let remaining_digits = self.board[x][y];
-        if remaining_digits.not_any() {
+        if remaining_digits == 0 {
             return Err(eyre!("No possible digits left in ({}, {})", x, y));
         }
 
@@ -61,65 +76,57 @@ impl Solver {
         let block_bit_index = (x % 3) + 3 * (y % 3);
 
         // Clear the reverse index
-        let mut mask: Digits = BitArray::ZERO;
-        for bit_index in digits_to_eliminate[..9].iter_ones() {
-            mask[..9].fill(true);
-            mask.set(y, false);
-            self.possible_cols[x][bit_index] &= mask;
-            if self.possible_cols[x][bit_index].not_any() {
+        iter_ones!(for bit_index in digits_to_eliminate => {
+            // Clear columns
+            self.possible_cols[x][bit_index] &= !(1 << y);
+            if self.possible_cols[x][bit_index] == 0{
                 return Err(eyre!("No possible digits left in col {}", x));
             }
 
-            mask[..9].fill(true);
-            mask.set(x, false);
-            self.possible_rows[y][bit_index] &= mask;
-            if self.possible_rows[y][bit_index].not_any() {
+            // Clear rows
+            self.possible_rows[y][bit_index] &= !(1 << x);
+            if self.possible_rows[y][bit_index] == 0{
                 return Err(eyre!("No possible digits left in row {}", y));
             }
 
-            mask[..9].fill(true);
-            mask.set(block_bit_index, false);
-            self.possible_blocks[block_x][block_y][bit_index] &= mask;
-            if self.possible_blocks[block_x][block_y][bit_index].not_any() {
+            // Clear blocks
+            self.possible_blocks[block_x][block_y][bit_index] &= !(1 << block_bit_index);
+            if self.possible_blocks[block_x][block_y][bit_index] == 0{
                 return Err(eyre!(
                     "No possible digits left in block row {} col {}",
                     y,
                     x
                 ));
             }
-        }
+        });
 
         // If only one digit is left, eliminate that digit from all peers
         if remaining_digits.count_ones() == 1 {
-            let remaining_digit_index = remaining_digits.first_one().unwrap();
+            let remaining_digit_index = remaining_digits.trailing_zeros();
 
             // Get all positions where this digit is set in the current row, column and block,
             // and eliminate them from those squares.
 
             // Eliminate from row
-            let mut remaining_position_mask = self.possible_rows[y][remaining_digit_index];
-            mask[..9].fill(true);
-            mask.set(x, false); // Don't eliminate from current square
-            remaining_position_mask &= mask;
-            for bit_index in remaining_position_mask[..9].iter_ones() {
+            let mut remaining_position_mask: DigitMask =
+                self.possible_rows[y][remaining_digit_index as usize];
+            remaining_position_mask &= !(1 << x);
+            iter_ones!(for bit_index in remaining_position_mask => {
                 self.eliminate(bit_index, y, remaining_digits)?;
-            }
+            });
 
             // Eliminate from column
-            remaining_position_mask = self.possible_cols[x][remaining_digit_index];
-            mask[..9].fill(true);
-            mask.set(y, false); // Don't eliminate from current square
-            remaining_position_mask &= mask;
-            for bit_index in remaining_position_mask[..9].iter_ones() {
+            remaining_position_mask = self.possible_cols[x][remaining_digit_index as usize];
+            remaining_position_mask &= !(1 << y);
+            iter_ones!(for bit_index in remaining_position_mask => {
                 self.eliminate(x, bit_index, remaining_digits)?;
-            }
+            });
 
             // Eliminate from block
-            remaining_position_mask = self.possible_blocks[block_x][block_y][remaining_digit_index];
-            mask[..9].fill(true);
-            mask.set(block_bit_index, false); // Don't eliminate from current square
-            remaining_position_mask &= mask;
-            for bit_index in remaining_position_mask[..9].iter_ones() {
+            remaining_position_mask =
+                self.possible_blocks[block_x][block_y][remaining_digit_index as usize];
+            remaining_position_mask &= !(1 << block_bit_index);
+            iter_ones!(for bit_index in remaining_position_mask => {
                 let x_offset = bit_index % 3;
                 let y_offset = bit_index / 3;
                 self.eliminate(
@@ -127,34 +134,33 @@ impl Solver {
                     block_y * 3 + y_offset,
                     remaining_digits,
                 )?;
-            }
+            });
         }
 
         // For each digit we just eliminated, find if it now only has one remaining posible location
         // in either the row, column or block. If so, set the digit
-        for bit_index in digits_to_eliminate[..9].iter_ones() {
+        iter_ones!(for bit_index in digits_to_eliminate => {
             // Check row
             if self.possible_rows[y][bit_index].count_ones() == 1 {
-                let digit_x = self.possible_rows[y][bit_index].first_one().unwrap();
+                let digit_x = self.possible_rows[y][bit_index].trailing_zeros() as usize;
                 self.set_digit(digit_x, y, bit_index)?;
             }
 
             // Check column
             if self.possible_cols[x][bit_index].count_ones() == 1 {
-                let digit_y = self.possible_cols[x][bit_index].first_one().unwrap();
+                let digit_y = self.possible_cols[x][bit_index].trailing_zeros() as usize;
                 self.set_digit(x, digit_y, bit_index)?;
             }
 
             // Check column
             if self.possible_blocks[block_x][block_y][bit_index].count_ones() == 1 {
                 let bit_with_digit = self.possible_blocks[block_x][block_y][bit_index]
-                    .first_one()
-                    .unwrap();
+                    .trailing_zeros() as usize;
                 let x_offset = bit_with_digit % 3;
                 let y_offset = bit_with_digit / 3;
                 self.set_digit(block_x * 3 + x_offset, block_y * 3 + y_offset, bit_index)?;
             }
-        }
+        });
 
         // While we're here, check if the square has a popcnt of 2, this means it's has the minimum
         // number of possibilities without being "done", making it an excellent candidate for the next
@@ -167,14 +173,9 @@ impl Solver {
     }
 
     pub fn set_digit(&mut self, x: usize, y: usize, digit: usize) -> Result<()> {
-        let mut digit_mask: Digits = BitArray::ZERO;
-        digit_mask.set(digit, true);
-
-        assert!((self.board[x][y] & digit_mask).count_ones() > 0);
-
-        self.board[x][y].set(digit, true);
-
-        self.eliminate(x, y, !digit_mask & ONES.get().unwrap())
+        let digit_mask = 1 << digit;
+        self.board[x][y] |= digit_mask;
+        self.eliminate(x, y, !digit_mask)
     }
 
     pub fn get_next_square_to_assign(self) -> Option<(usize, usize)> {
@@ -212,15 +213,15 @@ impl Solver {
                 }
 
                 // Make sure this digit isn't in any of the peers
-                let digit = self.board[x][y].first_one().unwrap();
+                let digit = self.board[x][y].trailing_zeros();
                 for _x in 0..9 {
-                    if _x != x && self.board[x][y].first_one().unwrap_or(10) == digit {
+                    if _x != x && self.board[x][y].trailing_zeros() == digit {
                         return false;
                     }
                 }
 
                 for _y in 0..9 {
-                    if _y != y && self.board[x][y].first_one().unwrap_or(10) == digit {
+                    if _y != y && self.board[x][y].trailing_zeros() == digit {
                         return false;
                     }
                 }
@@ -232,8 +233,7 @@ impl Solver {
 
                 for _x in (start_x..).take_while(|i| *i < end_x) {
                     for _y in (start_y..).take_while(|i| *i < end_y) {
-                        if _x != x && _y != y && self.board[x][y].first_one().unwrap_or(10) == digit
-                        {
+                        if _x != x && _y != y && self.board[x][y].trailing_zeros() == digit {
                             return false;
                         }
                     }
@@ -246,11 +246,11 @@ impl Solver {
 
 impl From<&Board> for Solver {
     fn from(value: &Board) -> Self {
-        let board = [[bitarr![1; 9]; 9]; 9];
+        let board = [[0x1ff; 9]; 9];
 
-        let possible_blocks = [[[bitarr![1; 9]; 9]; 3]; 3];
-        let possible_rows = [[bitarr![1; 9]; 9]; 9];
-        let possible_cols = [[bitarr![1; 9]; 9]; 9];
+        let possible_blocks = [[[0x1ff; 9]; 3]; 3];
+        let possible_rows = [[0x1ff; 9]; 9];
+        let possible_cols = [[0x1ff; 9]; 9];
 
         let mut solver = Solver {
             board,
@@ -260,10 +260,8 @@ impl From<&Board> for Solver {
             next_square_candidate: None,
         };
 
-        ONES.get_or_init(|| bitarr![1, 1, 1, 1, 1, 1, 1, 1, 1]);
-
-        for (y, row) in (*value).squares().iter().enumerate() {
-            for (x, cell) in row.iter().enumerate() {
+        for (x, row) in (*value).squares().iter().enumerate() {
+            for (y, cell) in row.iter().enumerate() {
                 if *cell > 0 {
                     let adjusted_cell = *cell - 1;
                     solver
@@ -276,12 +274,28 @@ impl From<&Board> for Solver {
     }
 }
 
+impl Debug for Solver {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for row in self.board.iter() {
+            for cell in row.iter() {
+                if cell.count_ones() == 1 {
+                    let val = cell.trailing_zeros() + 1;
+                    write!(f, "{}", val)?;
+                } else {
+                    write!(f, "x")?;
+                }
+            }
+        }
+        Ok(())
+    }
+}
+
 impl Display for Solver {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "{}", UnicodeBox::Top)?;
 
-        for y in 1..=9 {
-            if y % 4 == 0 {
+        for y in 0..9 {
+            if y > 0 && y % 3 == 0 {
                 writeln!(f, "{}", UnicodeBox::Horizontal)?;
             }
 
@@ -290,16 +304,16 @@ impl Display for Solver {
                     write!(f, "{}", UnicodeBox::Vertical)?;
                 }
 
-                let cell = self.board[x][y - 1];
+                let cell = self.board[x][y];
                 if cell.count_ones() == 1 {
-                    let val = cell.first_one().unwrap() + 1;
+                    let val = cell.trailing_zeros() + 1;
                     write!(f, "{}", val.yellow())?;
                     continue;
                 } else {
-                    let remaining = cell[..8].load_be::<u32>();
+                    let remaining = cell & 0xFF;
 
-                    let cell_repr = char::from_u32(0x2800 + remaining).unwrap();
-                    if *cell.get(8).unwrap() {
+                    let cell_repr = char::from_u32((0x2800 + remaining).into()).unwrap();
+                    if cell & 0x100 == 0x100 {
                         write!(f, "{}", cell_repr.green())?;
                     } else {
                         write!(f, "{}", cell_repr.blue())?;
